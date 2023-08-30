b0382cab9ffc70e428236c91a30d87aad2204530
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-08-30 12:16:48 +0530

    login frontend updated

---
+++ firebase_app_login/lib/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h * 0.3,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("img/login.jpg"))),
+          )
+        ],
+      ),
+    );
+  }
+}
